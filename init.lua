5d748b621c9eb39da002a67051b1160579bdc368
araminian <armin.aminian@trivago.com>
2024-07-05 13:27:45 +0200

    Add more configurations

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Copilot
+vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false,
+})
+vim.g.copilot_no_tab_map = true
+
+-- ToggleTerm
+vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-/>", "<C-\\><C-n>:ToggleTerm<CR>", { noremap = true, silent = true })
